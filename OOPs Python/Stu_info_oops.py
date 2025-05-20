b583b3a1abf13b69229f08b2695bb534a66503dd
Balram Kushwah <bkushwah4321@gmail.com>
2025-05-20 13:10:01 +0530

    Files added to Python

---
+++ OOPs Python/Stu_info_oops.py
@@ -0,0 +1,19 @@
+class student:
+    def info(name,roll,gender):
+        name = name
+        roll = roll
+        gender = gender
+        
+
+n = input("Enter name : ")
+r = int(input("Enter roll : "))
+gen = input("Enter genter : ")
+
+n = student()
+n.name =n
+n.roll=r
+n.gender=gen
+
+
+
+print(n.roll)
---
+++ OOPs Python/Sum_of_two_int_oops.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sum(self, num1, num2):
+        return num1+num2
+num1 = int(input("Enter 1 : "))
+num2 = int(input("Enter 2 : "))
+# a = Solution()
+b = Solution().sum(num1,num2)
+print(b)
